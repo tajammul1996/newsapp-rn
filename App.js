e88d9b948ab70140094ee6b58894cdedec7bd191
tajammul1996 <tajammul.pasha@accoliteindia.com>
2020-07-23 07:04:22 +0530

    added network call

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import { Text, View, SafeAreaView, StyleSheet, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, SafeAreaView, StyleSheet, Platform, FlatList, ActivityIndicator } from 'react-native';
 import NewsCard from "./src/components/NewsCard";
 
-const data = {
-  source: {
-    id: null,
-    name: "Global Times"
-  },
-  author: null,
-  title: "US-led anti-China alliance set for all-out failure - Global Times",
-  description: "The latest US attempt to build a “global anti-China alliance” is doomed to fail as Western countries don’t share its policy towards China. China needs to convince the rest of the world by enhancing opening-up and reform, and be more responsible on internatio…",
-  url: "https://www.globaltimes.cn/content/1195351.shtml",
-  urlToImage: "https://www.globaltimes.cn/Portals/0/attachment/2020/2020-07-22/05f8be82-0c4d-4966-ac0f-16a537fe966d.jpeg",
-  publishedAt: "2020-07-22T14:00:49Z",
-  content: "The latest US attempt to build a global anti-China alliance is doomed to fail as Western countries don't share its policy toward China. China needs to  convince the rest of the world by enhancing o… [+5507 chars]"
-}
-
 export default function App() {
+  const [articles, setArticles] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch("http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=242d96e64f34477ca3077bed18b69ddd");
+    const data = await response.json();
+    setArticles(data.articles);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const renderList = () => {
+    console.log(articles);
+    if (articles.length > 0) {
+      return (
+        <FlatList
+          data={articles}
+          renderItem={({ item }) => <NewsCard data={item} key={item.publishedAt} />}
+          initialNumToRender={5}
+        />
+      )
+    }
+    return <ActivityIndicator size="large" />
+  }
+
   return (
     <SafeAreaView style={styles.safeAreaStyle}>
-      <Text>Hello world!</Text>
-      <NewsCard data={data} />
+      {renderList()}
     </SafeAreaView>
   );
 }
